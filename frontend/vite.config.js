66ecf042bc817bfcffd08cab631090cdc4315671
gk07 <gk_520@hotmail.com>
2023-11-29 02:36:52 +0800

    update frontend settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,13 +14,11 @@ export default defineConfig({
     }
   },
   server: {
-    host: true,
-    port: 3000,
     proxy: {
       // 指定代理所有/api请求
       '/api': {
         // 代理请求之后的请求地址
-        target: 'http://backend:8000/',
+        target: 'http://backend/',
         // 跨域
         changeOrigin: true
       }
